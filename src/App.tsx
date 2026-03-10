6c868bb776685b195525b32a8061616209b73089
Dezrael <sergej31913@gmail.com>
2026-03-10 03:16:09 +0300

    Base url fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <nav className="App-nav">
           <Link to="/" className="nav-link">
